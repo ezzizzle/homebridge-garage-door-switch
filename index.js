856744369f1eff2a6e7c834b172e5cc1bdd1ddcd
Eric <ezzizzle@users.noreply.github.com>
2021-03-18 20:47:42 +1100

    Bumped version to 1.0.0

--- index.js
+++ index.js
@@ -168,7 +168,7 @@ GDSwitch.prototype.getServices = function() {
     .setCharacteristic(Characteristic.Manufacturer, 'GDSwitches')
     .setCharacteristic(Characteristic.Model, '001')
     .setCharacteristic(Characteristic.SerialNumber, 'GDS-001')
-    .setCharacteristic(Characteristic.FirmwareRevision, '0.0.3')
+    .setCharacteristic(Characteristic.FirmwareRevision, '1.0.0')
 
   let garageDoorService = new Service.GarageDoorOpener(this.name)
 
